7140118ed190afbf814db100c2af0ed31796acd9
Kyra Albaugh <231558924+kalbaugh05@users.noreply.github.com>
2025-10-02 02:19:55 +0000

    Add unit test for refactored sample_run_anonymizer

--- presidio-anonymizer/tests/test_sample.py
+++ presidio-anonymizer/tests/test_sample.py
@@ -2,5 +2,18 @@ import pytest
 from presidio_anonymizer.sample import sample_run_anonymizer
 
 def test_sample_run_anonymizer():
-    # replace the following line with your test
+    result = sample_run_anonymizer("My name is Bond.", 11, 15)
+
+    # Check anonymized text
+    assert result.text == "My name is BIP."
+
+    # Extract the operator result object
+    item = result.items[0]
+
+    # Verify its fields
+    assert item.start == 11
+    assert item.end == 14
+    assert item.entity_type == "PERSON"
+    assert item.text == "BIP"
+    assert item.operator == "replace"
     pass
